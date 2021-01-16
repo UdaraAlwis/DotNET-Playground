837e6a25068a378f92fda3ea076bb48019d0b6fd
Udara Alwis <udara.robotics@gmail.com>
2021-01-16 20:26:41 +0800

    CSharp9 Demo

---
+++ CSharp9DemoApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Hello World from .NET 5 and C# 9!");
+int value1 = 1;
+int value2 = 2;
+Console.WriteLine($"Adding Values {value1}+{value2}={AddTwoValues(value1, value2)}");
+
+static int AddTwoValues(int value1, int value2)
+{
+    return value1 + value2;
+}
